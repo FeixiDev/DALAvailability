63b3f52d3cf7e15f55a53ba392a4ec2aae367e46
shiki <237303802@qq.com>
2022-11-02 19:16:38 +0800

    修复了log无法正常写入，与执行错误命令，程序未退出的bug

--- log.py
+++ log.py
@@ -20,30 +20,30 @@ def get_host_ip():
 
 class Log(object):
     def __init__(self):
+
         now_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        self.log_path = sys.path[0]
-        self.file_name = self.log_path + str(now_time) + '.log'
+        self.file_name = str(now_time) + '.log'
         self.ip = get_host_ip()
 
 
     def write_to_log(self,data1,data2,level=10):
         logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        logger_format = (f"%(asctime)s - %(levelname)s - {self.ip} - {data1} - {data2}")
+        fmt = logging.Formatter(logger_format)
+        file_handler = logging.FileHandler(self.file_name)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(fmt)
+        logger.addHandler(file_handler)
         if level == 10:
-            level = logging.DEBUG
-        elif level ==20:
-            level = logging.INFO
+            logger.debug("this is debug")
+        elif level == 20:
+            logger.info("this is info")
         elif level == 30:
-            level = logging.WARNING
+            logger.error("this is error")
         elif level == 40:
-            level = logging.ERROR
+            logger.warning("this is warning")
         elif level == 50:
-            level = logging.CRITICAL
+            logger.critical("this is critical")
         else:
             print('The level must be 10,20,30,40,50')
-
-        fh = logging.FileHandler(filename=self.file_name, mode='a', encoding="UTF-8")
-        fh.setLevel(level)
-        formatter = logging.Formatter(f"%(asctime)s - %(levelname)s - {self.ip} - {data1} - {data2}")
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        return logger
